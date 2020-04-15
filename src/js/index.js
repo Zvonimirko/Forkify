82add2402ce34d0ea99577ea103af6369a26eb76
Zvonimir <44640961+Zvonimirko@users.noreply.github.com>
2020-04-15 20:01:45 +0200

    Rad na search i viewu

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,41 @@
-// Global app controller
-import num from './test';
+import Search from './models/Search';
 
-console.log(`I imported ${num} from another module called tst.js`);
+/* Global state of the app
+* - Search object
+* - Current recipe object
+* - Shopping list object
+* - Liked recipes
+*/
+
+const state = {};
+
+const controlSearch = async () => {
+    // 1. Get query from the view
+    const query = 'pizza' // todo
+
+    if (query) {
+        // 2. New search objecz and add to state
+        state.search = new Search(query);
+
+        // 3. Prepare UI for result
+
+        // 4. Search for recipes
+        await state.search.getResults();
+
+        // 5. Render results on UI
+        console.log(state.search.result);
+    }
+};
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
+
+// const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+
+// const res = await axios(`https://forkify-api.herokuapp.com/api/get?rId=${this.id}`);
